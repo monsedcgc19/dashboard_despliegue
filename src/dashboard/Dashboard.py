aa27124c31bd9c1f7fdb59b06a6156ff276d845d
monsedcgc19 <m.da@uniandes.edu.co>
2025-11-21 19:35:00 -0600

    Modificacion run de server

--- src/dashboard/Dashboard.py
+++ src/dashboard/Dashboard.py
@@ -344,4 +344,4 @@ def update_dashboard(date_str):
 # Run app
 # -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run_server(host="0.0.0.0", port=8050, debug=True)
